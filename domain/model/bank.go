914ecbadb1ef9f03557cf5272d9ade9a79e28e2d
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-16 12:15:45 -0300

    Add UUID and timestamps to bank model
    A UUID field and timestamps have been added to the bank model.
    A constructor, `NewBank`, has been introduced to make it easier to construct new instances of the Bank struct with an automatically assigned unique identifier and timestamps. This aims to reduce code repetition and potential errors across the application.

--- domain/model/bank.go
+++ domain/model/bank.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"time"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type Bank struct {
@@ -10,4 +12,16 @@ type Bank struct {
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+
+	bank.Id = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+
+	return &bank, nil
 }
