8970351b804bd8eb4ac1f907a0b9f7eaf653e8d9
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-16 12:09:17 -0300

    Add timestamps to Bank struct in model
    The change adds fields 'CreatedAt' and 'UpdatedAt' to the Bank struct in order to track the creation and update times of bank records. This provides a way to trace the history of changes made to the data in the Bank model.

--- domain/model/bank.go
+++ domain/model/bank.go
@@ -1,7 +1,13 @@
 package model
 
+import (
+	"time"
+)
+
 type Bank struct {
-	Id   string `json:"id"`
-	Code string `json:"code"`
-	Name string `json:"name"`
+	Id        string    `json:"id"`
+	Code      string    `json:"code"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
