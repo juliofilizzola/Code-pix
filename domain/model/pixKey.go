370f683d91f43712b9e6eee61ab3c6c2355ef226
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-16 14:44:00 -0300

    feat: Add pixKey model to domain
    A new Go file for the PixKey model was created under domain/model. This model includes validation for the required fields in the RESTful API response object including kind, key, accountId, and status.

---
+++ domain/model/pixKey.go
@@ -0,0 +1,10 @@
+package model
+
+type PixKey struct {
+	Base      `valid:"required"`
+	Kind      string   `json:"kind" valid:"notnull"`
+	Key       string   `json:"key" valid:"notnull"`
+	AccountID string   `json:"account_id" valid:"notnull"`
+	Account   *Account `json:"account" valid:"-"`
+	Status    string   `json:"status" valid:"notnull"`
+}
