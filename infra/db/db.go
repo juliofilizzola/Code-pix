060a032693dec0d7a2ce320089eb8b9f43cf7c23
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-18 02:24:22 -0300

    feat: Refactor db connection function for consistency
    Simplified database connection function by removing environment specific logic. Now using env package to manage environment variables. This provides a single way of fetching environment settings, improving the code maintainability.

--- infra/db/db.go
+++ infra/db/db.go
@@ -2,51 +2,30 @@ package db
 
 import (
 	"log"
-	"os"
-	"path/filepath"
-	"runtime"
 
 	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
+	"github.com/juliofilizzola/Code-pix/config/env"
 	"github.com/juliofilizzola/Code-pix/domain/model"
 	_ "github.com/lib/pq"
 	_ "gorm.io/driver/sqlite"
 )
 
-func init() {
-	_, b, _, _ := runtime.Caller(0)
-	baseUrl := filepath.Dir(b)
-
-	err := godotenv.Load(baseUrl + "/../../.env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env files")
-	}
-}
-
-func ConnectDB(env string) (*gorm.DB, error) {
-	var dsn string
-	var db *gorm.DB
+func ConnectDB() (*gorm.DB, error) {
 	var err error
+	var db *gorm.DB
 
-	if env != "test" {
-		dsn = os.Getenv("dsn")
-		db, err = gorm.Open(os.Getenv("dbType"), dsn)
-	} else {
-		dsn = os.Getenv("dsnTest")
-		db, err = gorm.Open(os.Getenv("dbTypeTest"), dsn)
-	}
+	db, err = gorm.Open(env.DbType, env.Dns)
 
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
 		return nil, err
 	}
 
-	if os.Getenv("debug") == "true" {
+	if env.Debug == "true" {
 		db.LogMode(true)
 	}
 
-	if os.Getenv("AutoMigrateDb") == "true" {
+	if env.AutoMigrateDb == "true" {
 		db.AutoMigrate(&model.Bank{}, &model.Account{}, &model.PixKey{}, &model.Transaction{})
 	}
 
