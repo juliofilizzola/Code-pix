99c757c0d851c929b30860b6fbe55c360bb30d8e
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-18 02:45:01 -0300

    feat: Add Transaction and Pix use cases
    Created new 'TransactionUseCase' and 'PixUseCase' in 'transaction.go' and 'pix.go' respectively. These changes include methods for registering and finding transactions and pix keys. Furthermore, transaction status confirming, completing, and error-handling methods have also been added. This development is part of the development of the business logic allowing for more structured and organized code.

---
+++ app/usecase/pix.go
@@ -0,0 +1,38 @@
+package usecase
+
+import (
+	"github.com/juliofilizzola/Code-pix/domain/model"
+)
+
+type PixUseCase struct {
+	PixKeyRepository model.PixKeyRepositoryInterface
+}
+
+func (p *PixUseCase) RegisterKey(key string, kind string, accountId string) (*model.PixKey, error) {
+	account, err := p.PixKeyRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	pixKey, err := model.NewPixKey(kind, key, account)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = p.PixKeyRepository.Register(pixKey); err != nil {
+		return nil, err
+	}
+	if pixKey.ID == "" {
+		return nil, err
+	}
+
+	return pixKey, nil
+}
+
+func (p *PixUseCase) FindKey(key string, kind string) (*model.PixKey, error) {
+	pixKey, err := p.PixKeyRepository.FindKeyByKind(key, kind)
+	if err != nil {
+		return nil, err
+	}
+	return pixKey, nil
+}
